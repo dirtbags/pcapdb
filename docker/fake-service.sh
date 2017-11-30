a5cca81a07fce11b9abc2fbe84d129fed7928612
Neale Pickett <neale@lanl.gov>
2017-11-30 17:01:12 +0000

    Clean docker image build, but it will not run

---
+++ docker/fake-service.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# In my defense, builds take a bit of time.
+
+echo -n "[$*] "
+shuf -n 1 <<EOD
+You got it, big guy!
+Anything you say, boss!
+I'll get right on that!
+Consider it done!
+EOD
