7378bcbeae81db936b1a3cf03b35d0366cb43363
Neale Pickett <neale@lanl.gov>
2017-12-19 23:52:17 +0000

    Got postgres bind mounts working

--- docker/README.md
+++ docker/README.md
@@ -13,12 +13,26 @@ is the parent directory.
 Running It
 ----------
 
-    docker run --rm -it --network=host -e PCAPDB_HOSTNAME=$(hostname -f) pcapdb
+This assumes you want to bind-mount `/srv/pcapdb`
+
+    PCAPDB_BASE=/srv/pcapdb
+    PCAPDB_HOSTNAME=$(hostname -f) export PCAPDB_HOSTNAME
+    PCAPDB_MAILHOST=mail.example.com export PCAPDB_MAILHOST
+    docker run -d --name pcapdb --network=host -e PCAPDB_HOSTNAME=$PCAPDB_HOSTNAME -e PCAPDB_MAILHOST=$PCAPDB_MAILHOST -v $PCAPDB_BASE/etc:/var/pcapdb/etc -v $PCAPDB_BASE/postgresql:/var/lib/postgresql/data pcapdb
 
 It will start up on port 443 of the host network interface.
+
+Next you will need to create your administrative account:
+
+    docker exec pcapdb sudo -u capture bin/python core/manage.py add_user admin Ad Ministrator you@example.com
+
+Click on the link in your email to set a password and finish account creation.
+
+
 
 TODO
 ----
 
+* Do we need `--network=host` to capture packets? If not, remove, so the ops person can decide how they want to deploy.
 * Add to "running it" how to bind-mount whatever needs bind-mounting
 * Does docker run need a capability?
--- docker/pcapdb-omnibus-init.sh
+++ docker/pcapdb-omnibus-init.sh
@@ -6,9 +6,25 @@ log () {
   echo "[Omnibus Init] $*" 1>&2
 }
 
-start () {
-  log "Starting $1..."
+
+log "Writing configuration file"
+cfgset () {
+  cfg=$1
+  var=$2
+  val=${3:-MISSING_VARIABLE_NAME}
+  if [ -z "$val" ]; then
+    echo "ERROR: \$$var must be set!" 1>&2
+    exit 1
+  fi
+  
+  sed -i "s/^#\?\($cfg\) \?=.*/\1 = $val/" /var/pcapdb/etc/pcapdb.cfg
 }
+cfgset allowed_hosts "$PCAPDB_HOSTNAME" PCAPDB_HOSTNAME
+cfgset search_head_host "$PCAPDB_HOSTNAME" PCAPDB_HOSTNAME
+cfgset host "$PCAPDB_MAILHOST" PCAPDB_MAILHOST
+cfgset capture_node true
+cfgset search_head true
+
 
 log "Setting up directories and permissions"
 : >> /var/pcapdb/log/django.log
@@ -16,30 +32,38 @@ chown capture /var/pcapdb/log/django.log
 
 # XXX: Tech Debt: this is not a great way to start these services. Maybe that doesn't matter, though?
 
+
 if [ ! -f /etc/ssl/pcapdb.pem ]; then
   log "Generating /etc/ssl/pcapdb.pem"
   openssl req -nodes -x509 -newkey rsa:4096 -days 9999 -keyout /etc/ssl/pcapdb.key -out /etc/ssl/pcapdb.pem \
   -subj '/CN=pcapdb'
 fi
 
-start NGINX
+
+log "Starting NGINX"
 nginx
 
-start PostgreSQL
-pg_ctlcluster 9.5 main start
+PGBIN=/usr/lib/postgresql/9.5/bin
+if [ ! -f $PGDATA/PG_VERSION ]; then
+  log "Setting up PostgreSQL"
+  sudo -u postgres $PGBIN/pg_ctl -D $PGDATA init
+fi
 
-start RabbitMQ
+log "Starting PostgreSQL"
+sudo -u postgres $PGBIN/pg_ctl -D $PGDATA start
+
+
+log "Starting RabbitMQ"
 rabbitmq-server &
 
-# XXX: what is a more appropriate place for initialized?
+
+# XXX: what is a more appropriate directory for `initialized`?
 if [ ! -f /var/pcapdb/initialized ]; then
   log "Running initial setup..."
   yes yes | /var/pcapdb/core/bin/post-install.sh -s -c 127.0.0.1
   touch /var/pcapdb/initialized
 fi
 
-log "Writing configuration file"
-[ -n "$PCAPDB_HOSTNAME" ] && sed -i 's/.*allowed_hosts =.*/allowed_hosts = '"$PCAPDB_HOSTNAME"'/' /var/pcapdb/etc/pcapdb.cfg
 
-start Supervisor
+log "Starting Supervisor"
 supervisord -n -c /etc/supervisor/supervisord.conf
